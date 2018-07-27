f8864862cb19189e81b4d250e703b2d37dc5fb3e
Scarlett Samantha Verheul <scarlett@arcane-tech.nl>
2018-07-27 14:19:44 +0200

    [BUG] Fixed some filter issues

--- static/js/app.js
+++ static/js/app.js
@@ -2,8 +2,8 @@ val_prev_count = 0;
 
 function clear_filter(filter_bar) {
     filter_bar.val('');
-    jQuery(".filterable").find('tr').filter(function(i, v){
-        return true;
+    jQuery(".filterable").find('tr').each(function(e){
+        jQuery(this).show()
     });
 }
 
@@ -14,7 +14,7 @@ $(document).ready(function(e){
             return $(elem).text().toUpperCase().indexOf(arg.toUpperCase()) >= 0;
         };
     });
-    jQuery('.clear_filter_btn').click(function(e){
+    jQuery('#clear_filter').click(function(e){
         clear_filter(jQuery('#searchInput'));
     });
 
@@ -29,7 +29,30 @@ $(document).ready(function(e){
     $('#search_form').submit(function(e){
         jQuery(this).attr('action', '/search/' + jQuery('#search_query').val() + '/' + jQuery('#search_param').val())
     });
-    $("#searchInput").change(function () {
+    $("#searchInput").keyup(function(){
+        current_val = jQuery(this).val();
+        if(current_val.length > 1 || current_val === '' || val_prev_count > current_val.length) {
+            val_prev_count = current_val.length;
+            //split the current value of searchInput
+            var data = this.value.split(" ");
+            //create a jquery object of the rows
+            var jo = $(".filterable").find("tr");
+            if (this.value === "") {
+                jo.show();
+                return;
+            }
+            jo.hide();
+            jo.filter(function (i, v) {
+                var $t = $(this);
+                for (var d = 0; d < data.length; ++d) {
+                    if ($t.is(":contains('" + data[d] + "')")) {
+                        return true;
+                    }
+                }
+                return false;
+            }).show();
+        }
+    }).change(function () {
         current_val = jQuery(this).val();
         if(current_val.length > 1 || current_val === '' || val_prev_count > current_val.length) {
             val_prev_count = current_val.length;
